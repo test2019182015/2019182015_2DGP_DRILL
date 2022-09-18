d48cfb015a92f0d88aa0ad5cfe928e5bcbeca904
test2019182015 <112798284+test2019182015@users.noreply.github.com>
2022-09-18 15:35:19 +0900

    DRILL 4번

---
+++ drill4.py
@@ -0,0 +1,40 @@
+import turtle
+
+count=4
+while(count>0):
+  turtle.right(90)
+  turtle.forward(500)
+  count -=1
+
+count=5
+while(count>0):
+  if(count%2==0):
+   turtle.left(90)
+   turtle.forward(100)
+   turtle.left(90)
+   turtle.forward(500)
+  elif(count%2!=0):
+   turtle.right(90)
+   turtle.forward(100)
+   turtle.right(90)
+   turtle.forward(500)
+  count -=1
+
+turtle.penup()
+turtle.goto(0,-500)
+turtle.pendown()
+
+count=5
+while(count>0):
+  if(count%2==0):
+   turtle.left(90)
+   turtle.forward(500)
+   turtle.right(90)
+   turtle.forward(100)
+  elif(count%2!=0):
+   turtle.right(90)
+   turtle.forward(500)
+   turtle.left(90)
+   turtle.forward(100)
+  count -=1
+
