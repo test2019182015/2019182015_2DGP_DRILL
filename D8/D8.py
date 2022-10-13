11ee1ef188a5eaeb0dc2e81c48ae95eb712967ff
test2019182015 <112798284+test2019182015@users.noreply.github.com>
2022-10-13 12:59:43 +0900

    move

--- D8/D8.py
+++ D8/D8.py
@@ -238,4 +238,16 @@ def handle_events():
                     stay_left=1
                     run_up_down_left=0
                     run_up_down_right=0
-
+running = True
+x = 800 // 2
+frame = 0
+dir = 0
+while running:
+clear_canvas()
+grass.draw(400, 30)
+character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+update_canvas()
+handle_events()
+frame = (frame + 1) % 8
+x += dir * 5
+delay(0.01)
--- gm/play.py
+++ gm/play.py
@@ -1,10 +1,15 @@
 import random
 from pico2d import *
+from dataclasses import dataclass
 
 back_WIDTH, back_HEIGHT = 600, 600
 
-
-
+run_move=0
+stay_move=0
+run_jump=0
+move_dir=0
+move_to=0
+jump_to=0
 # 710, 896
 
 def draw_back():
@@ -15,87 +20,163 @@ def draw_back():
 
 
 def run_draw_move():
-    global y
-    global x
+    print('move')
     global frame
     draw_back()
-    character.clip_draw(frame * 100, 100, 100, 100, x, y)
-    update_canvas()
+    if move_to == 0: #left run
+        character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+        boshy_ch.X+=move_dir*7
+        update_canvas()
+        frame = (frame + 1) % 6
+    #                      프레임      줄   크기   높이   x   y    w   h
+    # 줄 128의 배수
+    elif move_to == 1: #right run
+        character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+        boshy_ch.X += move_dir*7
+        update_canvas()
+        frame = (frame + 1) % 6+6
     handle_events()
-    x+=dir*5
-    frame = (frame + 1) % 8
 
     delay(0.01)
 
 def stay_draw():
-     delay(1)
+    global frame
+    draw_back()
+    if move_to == 0:  # left run
+        frame = 1
+        character.clip_draw(frame * 128, 1280, 128, 130,boshy_ch.X, boshy_ch.Y, 30, 30)
+        update_canvas()
+    #                      프레임      줄   크기   높이   x   y    w   h
+    # 줄 128의 배수
+    elif move_to == 1:  # right run
+        frame = 7
+        character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+        update_canvas()
+        update_canvas()
+    handle_events()
+    delay(0.01)
 def run_draw_up():
-    delay(1)
+    print('jump')
+    global frame
+    for boshy_ch.Y in range(boshy_ch.Y,boshy_ch.Y+100,4):
+        delay(0.01)
+        draw_back()
+        if move_to == 0:  # left run
+            character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+            update_canvas()
+            frame = (frame + 1) % 6
+    #                      프레임      줄   크기   높이   x   y    w   h
+    # 줄 128의 배수
+        elif move_to == 1:  # right run
+            character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+            update_canvas()
+            update_canvas()
+            frame = (frame + 1) % 6 + 6
+
+    for boshy_ch.Y in range(boshy_ch.Y,boshy_ch.Y-100,-4):
+        delay(0.01)
+        draw_back()
+        if move_to == 0:  # left run
+            character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+            update_canvas()
+            frame = (frame + 1) % 6
+    #                      프레임      줄   크기   높이   x   y    w   h
+    # 줄 128의 배수
+        elif move_to == 1:  # right run
+            character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+            update_canvas()
+            update_canvas()
+            frame = (frame + 1) % 6 + 6
+
+    handle_events()
+    delay(0.1)
 def run():
-    if run_move==1:
+    if run_move == 1:
         run_draw_move()
-    elif run_jump==1:
+    if run_jump == 1:
         run_draw_up()
-    elif stay_move==1:
+    if stay_move == 1:
         stay_draw()
 
 
+def start_run():
+    global frame
+    draw_back()
+    frame = 7
+    character.clip_draw(frame * 128, 1280, 128, 130, boshy_ch.X, boshy_ch.Y, 30, 30)
+    update_canvas()
+
 def handle_events():
      global running
-
+     global run_move
+     global move_to
+     global move_dir
+     global stay_move
+     global run_jump
      events = get_events()
      for event in events:
          if event.type == SDL_QUIT:
              running = False
          elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
+                stay_move=0
+                run_move = 1
+                move_to = 1
+                move_dir +=1
                 run()
-            elif event.key == SDLK_LEFT:
+            if event.key == SDLK_LEFT:
+                stay_move=0
+                run_move = 1
+                move_to=0
+                move_dir -=1
                 run()
-            elif event.key == SDLK_UP:
+            if event.key == SDLK_UP:
+                stay_move = 0
+                run_jump=1
                 run()
             elif event.key == SDLK_ESCAPE:
                 running = False
          elif event.type == SDL_KEYUP:
               if event.key == SDLK_RIGHT:
+                  move_dir -=1
+                  run_move = 0
+                  stay_move=1
                   run()
               elif event.key == SDLK_LEFT:
+                  move_dir += 1
+                  run_move = 0
+                  stay_move = 1
                   run()
               elif event.key == SDLK_UP:
-                  run()
+                  run_jump = 0
+                  stay_move = 1
 
 
 
 
 
 
+class boshy_ch:
+    X: int = 30
+    Y: int = 30
+    move_to:int =0
 
-
-
-
-run_move=0
-stay_move=0
-run_jump=0
-move_dir=0
-jump_dir=0
-move_to=0
-jump_to=0
 open_canvas(back_WIDTH, back_HEIGHT)
 
 grass=load_image('grass1.png')
 TUK_ground = load_image('bg2.png')
+
 character = load_image('bosh.png')
+
 running = True
 frame=0
+
+start_run()
 while running:
-    draw_back()
-    character.clip_draw(frame * 128, 1152, 128, 130, 330, 125, 30, 30)
-    #                      프레임      줄   크기   높이   x   y    w   h
-    #줄 128의 배수
-    update_canvas()
-    frame = (frame + 1) % 4
+    run()
     handle_events()
-    delay(0.)
+
+    delay(0.03)
 
 close_canvas()
 
